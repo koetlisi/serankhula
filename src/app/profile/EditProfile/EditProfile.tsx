363c2a5997774a4ef44d1bf54d6a9b3a3263da47
Koetlisi Theko <45929051+koetlisi@users.noreply.github.com>
2024-08-13 17:06:39 +0200

    "@inertiajs/react";

--- src/app/profile/EditProfile/EditProfile.tsx
+++ src/app/profile/EditProfile/EditProfile.tsx
@@ -1,14 +1,13 @@
 import './editprofile.css.scss'
-import {Head} from "@inertiajs/react";
+
 import {NavBar} from "@/components/Components/NavThings/NavBar";
 import {Sidebar} from "@/components/Components/SideBar/Sidebar";
 import { Button, Tabs } from 'antd';
-import {TabItems} from "@/Pages/Profile/EditProfile/tab-items";
+import {TabItems} from "@/app/profile/EditProfile/tab-items";
 
 export const EditProfile = () => {
     const operations = <Button>Save Changes</Button>;
     return <div className="edit-profile">
-        <Head title="Edit Profile"/>
         <NavBar/>
         <div className="edit-profile-wrapper">
             <Sidebar/>

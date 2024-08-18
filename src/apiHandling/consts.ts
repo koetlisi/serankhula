b3aef3e74140e1197133a05465f80a36a733a0cd
Koetlisi Theko <45929051+koetlisi@users.noreply.github.com>
2024-08-18 11:57:23 +0200

    git aad .

--- src/apiHandling/consts.ts
+++ src/apiHandling/consts.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const apiClient = axios.create({
     baseURL: 'http://127.0.0.1:8001', // on development before push
     // baseURL: 'https://fast-api-eskm4g3dcq-ue.a.run.app', // on production after a push
-}); 
+});
 
 export const apiClientNodejs = axios.create({
     baseURL: 'http://127.0.0.1:3001', // on development before push

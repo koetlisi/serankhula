b5fbefbd6ab25e8e39ece752a929ee60134b0138
Koetlisi Theko <45929051+koetlisi@users.noreply.github.com>
2024-08-25 20:47:09 +0200

    save

--- src/app/cvTemplates/custome/editResume.tsx
+++ src/app/cvTemplates/custome/editResume.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
+import {useSelector} from "react-redux";
+import {RootState} from "@/GlobalRedux/store";
 
 const EditResume: React.FC = () =>{
+    const {selectedResume} = useSelector((state: RootState) => state.system);
+    const {resumes} = useSelector((state: RootState) => state.resume);
     return <div className="">
 
     </div>

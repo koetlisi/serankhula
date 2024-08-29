d83f342451dc091209f5ece05e9714c3f8a59d35
Koetlisi Theko <45929051+koetlisi@users.noreply.github.com>
2024-08-29 19:04:19 +0200

    first

---
+++ app/lib/redux/hooks.tsx
@@ -0,0 +1,41 @@
+import {useEffect} from "react";
+import {AppDispatch, RootState, store} from "./store";
+import {loadStateFromLocalStorage, saveStateToLocalStorage,} from "./local-storage";
+import {TypedUseSelectorHook, useDispatch, useSelector} from "react-redux";
+import {deepMerge} from "../deep-merge";
+import {initialResumeState, setResume} from "./resumeSlice";
+import {Resume} from "./types";
+import {initialSettings, setSettings, Settings} from "./settingsSlice";
+
+export const useAppDispatch: () => AppDispatch = useDispatch;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+
+export const useSaveStateToLocalStorageOnChange = () => {
+  useEffect(() => {
+    return store.subscribe(() => {
+      saveStateToLocalStorage(store.getState());
+    });
+  }, []);
+};
+
+export const useSetInitialStore = () => {
+  const dispatch = useAppDispatch();
+  useEffect(() => {
+    const state = loadStateFromLocalStorage();
+    if (!state) return;
+    if (state.resume) {
+      const mergedResumeState = deepMerge(
+        initialResumeState,
+        state.resume
+      ) as Resume;
+      dispatch(setResume(mergedResumeState));
+    }
+    if (state.settings) {
+      const mergedSettingsState = deepMerge(
+        initialSettings,
+        state.settings
+      ) as Settings;
+      dispatch(setSettings(mergedSettingsState));
+    }
+  }, [dispatch]);
+};

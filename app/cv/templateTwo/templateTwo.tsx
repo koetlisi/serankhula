fd85c7a44865a75c7122528728090b490e115760
Koetlisi Theko <45929051+koetlisi@users.noreply.github.com>
2024-08-31 19:15:24 +0200

    start template two

--- app/cv/templateTwo/templateTwo.tsx
+++ app/cv/templateTwo/templateTwo.tsx
@@ -1,3 +1,8 @@
-export const TemplateTwo = () =>{
-    return <></>
+import {TempFrom} from "@/app/cv/templateTwo/froms/tempFrom";
+
+export const TemplateTwo = () => {
+    return <div className="grid grid-cols-1 bg-dot md:grid-cols-2 p-0 gap-10">
+        <TempFrom/>
+        <div></div>
+    </div>
 }

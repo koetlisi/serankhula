fcf8ea46969b4509c2af71ff179f2ed54608f5fc
Koetlisi Theko <45929051+koetlisi@users.noreply.github.com>
2024-09-04 22:33:29 +0200

    commit

--- app/dashboard/Feeds/Feeds.tsx
+++ app/dashboard/Feeds/Feeds.tsx
@@ -1,23 +1,32 @@
 import './feeds.css.scss'
-import React from "react";
+import React, {useEffect} from "react";
 import {Posts} from "@/app/dashboard/Feeds/Post/Post";
 import {Share} from "@/app/dashboard/Feeds/Share/Share";
 import {Stories} from "@/app/dashboard/Feeds/Stories/Stories";
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {RootState} from "@/app/lib/appRedux/store";
+import {getPost} from "@/app/lib/appRedux/thunks/post/post";
+import {shuffleArray} from "@/service/shuffleArray";
+import {Post} from "@/app/lib/types/post";
+
 interface Prop {
-    stories:boolean
+    stories: boolean
 }
+
 export const Feeds: React.FC<Prop> = ({stories = true}) => {
-    const {posts} = useSelector((state: RootState) => state.posts);
+    const { posts} = useSelector((state: RootState) => state.posts);
+    const dispatch = useDispatch();
+    useEffect(() => {
+        // @ts-ignore
+        dispatch(posts.length>0?()=>{}:getPost())
+    }, []);
     return <div className="feeds">
         <div className="feed-wrapper">
             {stories && <Stories/>}
             <Share/>
-            {posts.map((post)=>(
-                <Posts posts={post} key={post.id}/>
+            {posts.length > 0 && shuffleArray<Post>(posts).map((post) => (
+                <Posts posts={post} key={post.id} />
             ))}
-
         </div>
     </div>
 }
